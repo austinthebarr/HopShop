c9e501adde463228fd04faa22a829924db867026
Austin Barr <austinbarr@protonmail.com>
2018-09-07 11:12:31 -0700

    add styles to splash page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,21 @@ import { Switch, Route } from 'react-router-dom';
 import Welcome from './Welcome';
 
 const App = () => {
+  var style = {
+    position: 'fixed',
+    width: '100%',
+    height:' 100%',
+    left: '0',
+    top: '0',
+    display: 'flex'
+  }
   return (
     <div>
       <Switch>
-        <Route path='/'
-          component={Welcome} />
+        <div style ={style}>
+          <Route path='/'
+            component={Welcome} />
+        </div>
       </Switch>
     </div>
   );
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 
+
 const Welcome = () => {
-  let style = {
-    fontFamily:'sans'
-  }
+  
   
   return(
-    <div>
-      <h1 className='style'>Welcome</h1>
-      <button>Enter</button>
+    <div className='splash'>
+      <style jsx>{`
+    .splash {
+      background-color: #41463D;
+      color: #FFF;
+      font-family: Monospace;
+      width: 100%;
+      height: 100%;
+      text-align: center;
+    }
+    .center {
+      margin-top: 25%
+    }  
+`}</style>
+      <div className='center'>
+        <h1 className='style'>Welcome to HOPSHOP</h1>
+        <button>Enter</button>
+      </div>
     </div>
   );
 };

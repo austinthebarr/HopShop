b16ad6939544c3163bbbec62ab20c6a3ab7b441c
Austin Barr <austinbarr@protonmail.com>
2018-09-08 19:55:50 -0700

    added style to error page

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -4,10 +4,32 @@ import PropTypes from 'prop-types';
 
 
 const Error404 = (props) => {
+
   return (
-    <div> 
-      <h2>Bummer, the page {props.location.pathname} does not exist!</h2>
-      <Link to='/'><button className='btn btn-danger'>Home</button></Link>
+    <div className='splash'> 
+      <style jsx>{`  
+    @import url('https://fonts.googleapis.com/css?family=Gloria+Hallelujah');
+    .splash {
+      background-color: #3A3335;
+      color: #FFF;
+      font-family:  'Gloria Hallelujah', cursive;
+      width: 100%;
+      height: 100vh;
+      text-align: center;
+      background: cover;
+    }
+    .center {
+      padding-top: 25%;
+    }
+    .error {
+      color:#ED1C24;
+    } 
+      `}
+      </style>
+      <div className='center'>
+        <h2>Bummer, the page: <span className='error'>{props.location.pathname}</span> does not exist!</h2>
+        <Link to='/'><button className='btn btn-danger'>Home</button></Link>
+      </div> 
     </div>
   );
 };
